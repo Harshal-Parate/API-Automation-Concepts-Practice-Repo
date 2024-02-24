92a5144e658a3ae36e5cd52f6d97d5b948cf9662
Harshal-Parate <Harshalparate1@gmail.com>
2024-02-24 20:23:27 +0530

    testNG Annotations

---
+++ src/test/java/Concepts/annotations/Case002.java
@@ -0,0 +1,12 @@
+package Concepts.annotations;
+
+import org.testng.annotations.Test;
+
+public class Case002 {
+
+    @Test
+    public void test1() {
+        System.out.println("I'm test of case002 class");
+    }
+
+}
