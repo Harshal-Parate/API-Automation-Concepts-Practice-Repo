1dab2f3f25b8ed786c0aba3268c6a88826de985a
Harshal-Parate <Harshalparate1@gmail.com>
2024-02-24 19:55:27 +0530

    Assert J

--- src/test/java/org/example/POST/bookingCreation.java
+++ src/test/java/org/example/POST/bookingCreation.java
@@ -5,11 +5,13 @@ import RequestFormation.RequestSpecificationService;
 import RequestFormation.ResponseParsingService;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 //import static TestDataBuilder.CreateBookingPayloadBuilder.getBookingCreationPayload;
 import static FilePaths.CreateBookingFilePaths.getBookingCreationFile;
 import static FilePaths.Endpoints.*;
 import static FileReaders.JSONFileReader.payload;
+import static org.assertj.core.api.Assertions.*;
 
 public final class bookingCreation {
 
@@ -18,5 +20,17 @@ public final class bookingCreation {
         RequestSpecification requestSpecification = RequestSpecificationService.requestSpecification(getCreateBookingEndpoint());
         Response response = RequestFormationService.post(requestSpecification, payload(getBookingCreationFile()));
         String parsedResponse = ResponseParsingService.responseData(response);
+        //System.out.println(parsedResponse);
+        int bookingId = response.then().extract().path("bookingid");
+
+        // TestNG
+        Assert.assertNotNull(bookingId);
+
+        // AssertJ
+        // import static org.assertj.core.api.Assertions.*;
+        assertThat(bookingId).isZero();
+
+        System.out.println("bookingid-->" + bookingId);
+
     }
 }
