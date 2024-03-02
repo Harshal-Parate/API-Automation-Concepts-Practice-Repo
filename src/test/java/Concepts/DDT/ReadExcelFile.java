7915decc58406bd3b3456ea4b3d836381b3547cd
Harshal-Parate <Harshalparate1@gmail.com>
2024-03-02 11:26:07 +0530

    DDT

--- src/test/java/Concepts/DDT/ReadExcelFile.java
+++ src/test/java/Concepts/DDT/ReadExcelFile.java
@@ -12,13 +12,12 @@ public class ReadExcelFile {
     private static Sheet sheet;
     private static final String FILE_NAME = "src/test/resources/Rest Full Boker APIs.xlsx";
 
-    public static Object[][] getTestData(String sheetName) {
+    public static Object[][] getData(String sheetName) {
 
         try {
             FileInputStream file = new FileInputStream(FILE_NAME);
             sheet = WorkbookFactory.create(file).getSheet(sheetName);
-        } catch (Exception ignored) {
-        }
+        } catch (Exception ignored) {}
 
         Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
         for (int i = 0; i < sheet.getLastRowNum(); i++) {
@@ -29,13 +28,13 @@ public class ReadExcelFile {
         return data;
     }
 
-    @DataProvider
-    public Object[][] getData() {
-        return getTestData("Creds");
-    }
-
-    @Test(dataProvider = "getData", dataProviderClass = ReadExcelFile.class)
-    public void testLoginData(String username, String password) {
-        System.out.println(username + "--->" + password);
-    }
+//    @DataProvider
+//    public Object[][] getData() {
+//        return getTestData("Creds");
+//    }
+//
+//    @Test(dataProvider = "getData", dataProviderClass = ReadExcelFile.class)
+//    public void testLoginData(String username, String password) {
+//        System.out.println(username + "--->" + password);
+//    }
 }
